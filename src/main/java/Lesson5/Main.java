2b1f411ec80e9cefee6df6316fc09d8b6f9ad6e1
Antbel7 <antbel7@rambler.ru>
2021-05-31 01:23:05 +0300

    The eng lesson5

--- src/main/java/Lesson5/Main.java
+++ src/main/java/Lesson5/Main.java
@@ -2,12 +2,30 @@ package Lesson5;
 
 public class Main {
 
-    public static void main (String[] arg) {
+    public static void main(String[] arg) {
+        Worker[] workers = new Worker[5];
 
-        Worker worker1 = new Worker("Пушкин","Писатель","onegin@mail.ru",
-                55555,80000,37);
+        workers[0] = new Worker("Пушкин", "Писатель", "onegin@mail.ru",
+                "55555", 80000, 37);
+        workers[1] = new Worker("Гагарин", "Космонавт", "gagarin@mail.ru",
+                "121212", 100000, 34);
+        workers[2] = new Worker("Ургант", "Ведущий", "urgant@mail.ru",
+                "111111", 300000, 43);
+        workers[3] = new Worker("Пучков", "Блогер", "goblin@mail.ru",
+                "020202", 1000000, 59);
+        workers[4] = new Worker("Маканаги", "Актер", "interstelar@mail.ru",
+                "010101", 500000, 51);
 
+        System.out.println("Информация \n");
+
+
+
+        for (int i = 0; i < workers.length; i++) {
+            if (workers[i].getAge() >= 40) {
+                workers[i].base();
+                System.out.println();
+            }
+        }
 
     }
-
 }
--- src/main/java/Lesson5/Worker.java
+++ src/main/java/Lesson5/Worker.java
@@ -5,12 +5,12 @@ public class Worker {
     private String name;
     private String post;
     private String email;
-    private long number;
+    private String number;
     private int money;
     private int age;
 
     public Worker (String name, String post, String email,
-                   long number, int money, int age) {
+                   String number, int money, int age) {
         this.name = name;
         this.post = post;
         this.email = email;
@@ -20,8 +20,18 @@ public class Worker {
 
 
     }
+    public void base() {
 
+       System.out.printf("Фамилия: %s \n" +
+                          "Призвание: %s \n" +
+                           "email: %s \n" +
+                             "Номер телефона: %s \n" +
+                                "Получка: %s \n"+
+                                    "Возраст: %s \n",name, post, email, number, money,age );
+    }
 
-
+    public int getAge (){
+        return age;
+    }
 
 }
