8c3a89662d1d7768222d9dfd36d96786a8aafdf5
Antbel7 <antbel7@rambler.ru>
2021-06-16 22:37:50 +0300

    finished it off lesson8

---
+++ src/main/java/Lesson8/Robot.java
@@ -0,0 +1,14 @@
+package Lesson8;
+
+public class Robot {
+
+    private String name;
+
+    public Robot(String name){
+        this.name=name;
+    }
+
+    public void run(){
+        System.out.println();
+    }
+}
